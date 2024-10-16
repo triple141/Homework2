99b51a2ccbc3a47b578bd7806603582bb663cef3
triple141 <102752937+triple141@users.noreply.github.com>
2024-10-16 22:09:28 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,108 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //       savarjisho 1-2
+
+//        double c = input.nextDouble();
+//        double f;
+//
+//        f = (c*9)/5 + 32;
+//        System.out.println(f);
+//
+//        double f2 = input.nextDouble();
+//        double c2 = (5*(f-32))/9;
+//        System.out.println(c2);
+
+
+        //        savarjisho 3
+
+//        int year;
+//        year = input.nextInt();
+//
+//        if(year % 400 == 0)
+//        {
+//            System.out.println("nakiani welia");
+//        }
+//        else if(year%4 == 0 && year%100!=0)
+//        {
+//            System.out.println("nakiani welia");
+//        }
+//        else
+//        {
+//            System.out.println("araa nakiani weli");
+//        }
+
+
+        //          savarjisho 4
+
+//        double x,y;
+//
+//        x = input.nextInt();
+//        y = input.nextInt();
+//
+//        if (x>0 && y>0)
+//        {
+//            System.out.println("1/4");
+//        }
+//        else if (x>0 && y<0) {
+//            System.out.println("4/4");
+//        }
+//        else if (x<0 && y>0)
+//        {
+//            System.out.println("2/4");
+//        }
+//        else if (x<0 && y<0)
+//        {
+//            System.out.println("3/4");
+//        }
+//        else if (x==0 && y>0)
+//        {
+//            System.out.println("1/4 && 2/4");
+//        }
+//        else if (x==0 && y<0)
+//        {
+//            System.out.println("3/4 && 4/4");
+//        }
+//        else if (x<0 && y==0)
+//        {
+//            System.out.println("2/4 && 3/4");
+//        }
+//        else if (x>0 && y==0)
+//        {
+//            System.out.println("1/4 && 4/4");
+//        }
+//        else
+//        {
+//            System.out.println("1/4 && 2/4 && 3/4 && 4/4");
+//        }
+
+
+        //      savarjisho 5
+
+//        String str = input.nextLine();
+//
+//        double x = input.nextDouble(),y = input.nextDouble();
+//
+//        if(str.equals("+"))
+//        {
+//            System.out.println(x+y);
+//        }
+//        else if(str.equals("-"))
+//        {
+//            System.out.println(x-y);
+//        }
+//        else if(str.equals("*"))
+//        {
+//            System.out.println(x*y);
+//        }
+//        else
+//        {
+//            System.out.println(x/y);
+//        }
+
+    }
+}
